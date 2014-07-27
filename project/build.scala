be82b439bbd387df6d21fdef9e9c31af5e533e8a
xuwei-k <6b656e6a69@gmail.com>
2014-07-27 11:21:18 +0900

    scalaz 7.1.0-RC2

--- project/build.scala
+++ project/build.scala
@@ -150,7 +150,7 @@ object build extends Build {
     "applybuilder71", file("scalaz71")
   ).settings(
     baseSettings ++ Seq(
-      libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.1.0-RC1"
+      libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.1.0-RC2"
     ) : _*
   )
 
