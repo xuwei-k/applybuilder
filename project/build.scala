7377009fbccdcbc75adb194b639278e70b563fb4
xuwei-k <6b656e6a69@gmail.com>
2018-05-16 23:03:02 +0900

    Scala 2.13.0-M4

--- project/build.scala
+++ project/build.scala
@@ -115,7 +115,7 @@ object build {
       pushChanges
     ),
     scalaVersion := Scala210,
-    crossScalaVersions := Scala211 :: Scala210 :: "2.12.6" :: "2.13.0-M3" :: Nil,
+    crossScalaVersions := Scala211 :: Scala210 :: "2.12.6" :: "2.13.0-M4" :: Nil,
     organization := "com.github.xuwei-k",
     startYear := Some(2014),
     description := "scalaz.Apply builder",
